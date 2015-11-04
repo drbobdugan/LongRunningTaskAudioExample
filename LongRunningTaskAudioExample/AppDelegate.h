bd3f34efa9322ea5dc5e2c8bc578accd10b79bee
drbobdugan <bdugan@stonehill.edu>
2015-11-04 13:13:53 -0500

    Initial Commit

---
+++ LongRunningTaskAudioExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LongRunningTaskAudioExample
+//
+//  Created by Bob Dugan on 11/4/15.
+//  Copyright © 2015 Bob Dugan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
